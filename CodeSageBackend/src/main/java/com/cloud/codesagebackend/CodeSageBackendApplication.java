02f148e9f2ab306d02598cecbd669c45e0b7847c
Cloud0yyk <1853231597@qq.com>
2025-11-26 17:04:30 +0800

    initialize codesage

---
+++ CodeSageBackend/src/main/java/com/cloud/codesagebackend/CodeSageBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.codesagebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSageBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeSageBackendApplication.class, args);
+    }
+
+}
---
+++ CodeSageBackend/src/test/java/com/cloud/codesagebackend/CodeSageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.codesagebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSageBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
